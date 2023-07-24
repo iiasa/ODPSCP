7bddd2327f82b95fde534daf769db543748de67d
Martin Jung <jung@iiasa.ac.at>
2023-07-24 16:19:58 +0200

    Basic libraries and first upload

---
+++ lib.R
@@ -0,0 +1,9 @@
+# List of key libraries to load for server and ui scripts
+library(shiny)
+library(shinyjs)
+library(shinyWidgets)
+library(shinythemes)
+library(shinydashboard)
+library(yaml)
+library(DT)
+library(tidyverse)
